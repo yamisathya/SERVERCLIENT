6757635bf966082a9e61ab9eb5d19af6b16d16d5
Varsha <varshabhaskar96@gmail.com>
2018-03-27 14:57:12 +0530

    updation2

--- MyClient.java
+++ MyClient.java
@@ -3,3 +3,6 @@ import java.io.*;
  class MyClient{  
  public static void main(String args[])throws Exception{  
  Socket s=new Socket("localhost",3333);  
+ DataInputStream din=new DataInputStream(s.getInputStream());  
+ DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+ BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
