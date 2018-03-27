5080aad2b29377b03fac338710764c4037ebac04
Yami <sruthisathyan001@gmail.com>
2018-03-27 14:59:12 +0530

    updation4

--- MyServer.java
+++ MyServer.java
@@ -6,3 +6,5 @@ class MyServer{
     ServerSocket ss=new ServerSocket(3333);  
     Socket s=ss.accept();  
     DataInputStream din=new DataInputStream(s.getInputStream());  
+    DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
