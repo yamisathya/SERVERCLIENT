7e1befbd501ae9abfaaf49c53101cae266ab08cf
Yami <sruthisathyan001@gmail.com>
2018-03-27 14:51:36 +0530

    updation2

--- MyServer.java
+++ MyServer.java
@@ -1,2 +1,6 @@
 import java.net.*;  
-    import java.io.*;  
+import java.io.*; 
+ 
+class MyServer{  
+    public static void main(String args[])throws Exception{  
+    ServerSocket ss=new ServerSocket(3333);  
